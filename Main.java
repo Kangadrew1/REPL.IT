3d805a6077b1e9e6771bae0e67a52179fc10f0ac
Andrew Nguyen <71796871+Kangadrew1@users.noreply.github.com>
2022-11-01 01:00:10 +0000

    For zybooks cryptography and understanding

--- Main.java
+++ Main.java
@@ -1,40 +1,34 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    int i = 0;
-    System.out.println(i);
-    intOP(i);
-    String a = "Hello! AM I LOWER CASE?";
-    makeLower(a);
-    System.out.println(a);
-    System.out.println((Math.pow(-7,4)) % 5);
+    //CS 2050
+    //encryption formula: c = (m+k) mod N
+    //decryption formula: m = (c-k) mod N
+    System.out.println("begin cryptography...\n> ----------");
+    //N = 79 zybooks 9.8 exercise
+    //k = 43
+    System.out.println("encrypting 62...");
+    encrypt(62, 43);
+    System.out.println("Received c = 26. decrypting... <(0.0<)");
+    decrypt(26, 43);
+    System.out.println("Received c = 12. decrypting... <(0.0<)");
+    decrypt(12,43);
+    //System.out.println((1117 - 2191) % 3391);
+    //System.out.println((-17) % 79);
   }
 
-protected static void intOP(int i) {
-    i++;
-    System.out.printf("i is %d%n", i);
-    i += 3;
-    System.out.printf("Now i is %d%n", i);
+  public static void encrypt(int m, int k) {
+    int c = (m + k) % 79;
+    System.out.println("The encryption is: " + c);
   }
 
-//see what happens here and why doesn't method work
-//as would think by name
-//b only exists within the scope of this variable
-//therefore a isn't modified 
-public static void makeLower(String b) {
-    b = b.toLowerCase();
-  } 
+  public static void decrypt(int c, int k) {
+    int a = c - k;
+    int m = (a % 79 + 79) % 79;
+    //this one works above the below is the formula to resolve negative mod in Java
+    //int m = (c % k + k) % k % k;
 
-//how to get the intended effect I found the fix I wanna say this is from 1331 looks like a 1331 exam question I tripped up on #RickyLandry
-//change line 8 or wherever it is to be a = makeLower(a) AND change method to return a string
-  //the issue was that the String operations only happen within the scope and do not automatically save unlike ints and other variable types. Strings are Special.
-/*
-  public static String makeLower(String b) {
-    b = b.toLowerCase();
-    return b;
-  } 
-*/
-  
-  //end of code as of 10-15-2022 (also just watched Iron Man 3—so feeling smart, genius, and witty—earlier because wifi was being baloney and/or I need to let my macbook pro finally have the full shutdown I think it's been a long time, like since June 2022 now, since I let it full shut down and boot up)
+    //the above for some reason doesn't work with what's in line with the one from zybooks. this doesn't do negative mod :(
+    System.out.println("The decryption is: " + m);
+  }
 }
-
